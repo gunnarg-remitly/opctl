236a3ff7c760edc33ab2dcdb7325fc3c41119e67
Cameron Little <cameron@remitly.com>
2021-03-30 17:17:57 +0200

    Remove some unused code

--- sdks/go/model/provider.go
+++ sdks/go/model/provider.go
@@ -4,11 +4,6 @@ import (
 	"context"
 )
 
-const (
-	OpFileName       = "op.yml"
-	DotOpspecDirName = ".opspec"
-)
-
 // DataProvider is the interface for something that provides data
 type DataProvider interface {
 	Label() string
